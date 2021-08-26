3734e7ad5805d23ad0082efca39fcc66167dfeba
Koshcey <DooglasMc@protonmail.com>
2021-08-26 22:23:01 +0300

    Add section about_me

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//const result = confirm("Dolbaeb?");
+//console.log(result);
+//const answer = prompt("18 est?","");
+//console.log(answer);
+
+const answers = [];
+answers [0] = prompt("Skolko let?","");
+answers [1] = prompt("Skolko let?","");
+answers [2] = prompt("Skolko let?","");
+answers [3] = prompt("Skolko let?","");
+
+
+
+
+
+ 
